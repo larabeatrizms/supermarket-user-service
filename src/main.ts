913c19b0dd4e51db5dfc6d3eea2cde16fb037d7b
Lara Beatriz <31808723+larabeatrizms@users.noreply.github.com>
2022-10-22 01:53:06 -0300

    chore: log port env

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,9 @@ async function bootstrap() {
 
   await app.listen();
 
-  logger.log('User Service is listening', 'Initialization');
+  logger.log(
+    `User Service is listening on port ${process.env.PORT}`,
+    'Initialization',
+  );
 }
 bootstrap();
