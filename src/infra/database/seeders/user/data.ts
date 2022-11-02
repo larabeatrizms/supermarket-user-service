7c94be583370be7cb1abcc4a147a12b83b9c21bf
Lara Beatriz <31808723+larabeatrizms@users.noreply.github.com>
2022-11-02 17:30:54 -0300

    feat: adds dotenv and adjustment seeder

--- src/infra/database/seeders/user/data.ts
+++ src/infra/database/seeders/user/data.ts
@@ -1,12 +1,16 @@
+import * as dotenv from 'dotenv';
+
 import { IUser } from 'src/modules/user/interfaces/user.interface';
+
+dotenv.config();
 
 export const users: IUser[] = [
   {
     id: 1,
     firstName: 'Admin',
-    lastName: 'Main',
+    lastName: process.env.ADMIN_LASTNAME,
     isAdmin: true,
-    password: 'admin',
-    email: 'lara.santos.033@ufrn.edu.br',
+    password: process.env.ADMIN_PASSWORD,
+    email: process.env.ADMIN_EMAIL,
   },
 ];
